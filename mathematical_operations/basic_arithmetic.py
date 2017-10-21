b9e69a9e7f8dd10d729650258e9808e0ca766c22
Abdulrazaq Ahmed <justforlearningcode@gmail.com>
2017-10-21 19:13:29 +0100

    Created basic_arithmetic.py file

---
+++ mathematical_operations/basic_arithmetic.py
@@ -0,0 +1,13 @@
+def main():
+    a = int(input("Enter your first number: "))
+    b = int(input("Enter your second number: "))
+    sum = a + b
+    product = a * b
+    difference = a - b
+    quotient = a / b
+    print("The sum of the two numbers is: " + str(sum))
+    print("The product of the two numbers is: " + str(product))
+    print("The difference between the two numbers is: " + str(difference))
+    print("The quotient of the two numbers is: " + str(quotient))
+    
+main()
